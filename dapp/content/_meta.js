d2d3358856daa9fb1cd50ff44025decf4bed0a91
Alive24 <xct24@live.com>
2026-01-11 18:07:08 +0800

    doc: Doc update

--- dapp/content/_meta.js
+++ dapp/content/_meta.js
@@ -1,6 +1,3 @@
 export default {
-  index: "CKBoost Docs",
-  "campaign-fetching-optimization": "Campaign Fetching Optimization",
-  "nostr-storage": "Nostr Storage",
-  "user-cell-management": "User Cell Management",
+  index: "CKBoost",
 };
